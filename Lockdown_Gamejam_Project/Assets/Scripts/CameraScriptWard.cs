f1062469f739a285a252a008c92d6117399e7d33
SexyStijn <ward.dheer@gmail.com>
2020-04-10 18:57:44 +0200

    Added my scripts xoxo

---
+++ Lockdown_Gamejam_Project/Assets/Scripts/CameraScriptWard.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScriptWard : MonoBehaviour
+{
+    [SerializeField] private Transform Player;
+    [SerializeField] private float  LerpValue;
+
+
+    private void FixedUpdate()
+  {
+      transform.position = Vector3.Lerp(transform.position,
+          new Vector3(Player.position.x, Player.position.y, transform.position.z), LerpValue);
+  }
+}
---
+++ Lockdown_Gamejam_Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rigidbody;
+    private bool _hasSquashed = true;
+    [SerializeField] private float _Squash = 0.2f;
+    [SerializeField] private float Frequency = 2f;
+
+    Vector2 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+
+        movement = movement.normalized;
+
+        if(movement.magnitude > 0.1f & _hasSquashed)
+        {
+            StartCoroutine(Squash());
+        }
+    }
+    private void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + movement * moveSpeed * Time.deltaTime);
+    }
+
+    IEnumerator Squash()
+    {
+        float time = 0;
+        _hasSquashed = false;
+
+
+        float sin = 0;
+
+        while (sin >= 0)
+        {
+             sin = _Squash * Mathf.Sin(time * Frequency);
+            transform.localScale = Vector3.one + new Vector3(-sin, sin, -sin);
+            time += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        _hasSquashed = true;
+        yield return null;
+    }
+}

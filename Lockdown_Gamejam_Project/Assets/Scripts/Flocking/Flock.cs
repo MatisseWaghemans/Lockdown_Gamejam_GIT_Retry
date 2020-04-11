8cee65cc489382ff334803570f6f1278ec9ec91c
SexyStijn <ward.dheer@gmail.com>
2020-04-11 10:48:55 +0200

    flock start

---
+++ Lockdown_Gamejam_Project/Assets/Scripts/Flocking/Flock.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flock : MonoBehaviour
+{
+    public Agent agentPrefab;
+    List<Agent> agents = new List<Agent>();
+    public Behaviour behaviour;
+
+    [Range(10, 500)]
+    public int startingCount = 250;
+    const float AgentDensity = 0.08f;
+
+    [Range(1f, 100f)]
+    public float driveFactor = 10f;
+    [Range(1f, 100f)]
+    public float MaxSpeed = 5f;
+    [Range(1f, 10f)]
+    public float neighborRadius = 1.5f;
+    [Range(0f, 1f)]
+    public float avoidanceRadiusMultiplier = 0.5f;
+
+    float squareMaxSpeed;
+    float squareNeighborRadius;
+    float squareAvoidanceRadius;
+    public float SquareAvoidanceRadius { get { return squareAvoidanceRadius; } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        squareMaxSpeed = MaxSpeed * MaxSpeed;
+        squareNeighborRadius = neighborRadius * neighborRadius;
+        squareAvoidanceRadius = squareNeighborRadius * avoidanceRadiusMultiplier * avoidanceRadiusMultiplier;
+
+        for (int i = 0; i < startingCount; i++)
+        {
+            Agent newAgent = Instantiate(
+                agentPrefab,
+                Random.insideUnitCircle * startingCount * AgentDensity,
+                Quaternion.Euler(Vector3.forward * Random.Range(0f, 360f)),
+                transform
+                );
+            newAgent.name = "Agent " + i;
+            agents.Add(newAgent);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

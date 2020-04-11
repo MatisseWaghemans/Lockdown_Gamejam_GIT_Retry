5e7bb2ad8b8e73e7c8423d6f4a7cc5792d00cdf0
SexyStijn <ward.dheer@gmail.com>
2020-04-11 14:59:17 +0200

    Revert "Revert "Flock""
    This reverts commit 5e636266ba48d068a99bc629548c4977a3fce00f.

---
+++ Lockdown_Gamejam_Project/Assets/Scripts/Flocking/SteeredCohesion.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Behavior/SteeredCohesion")]
+public class SteeredCohesion : Behaviour
+{
+    Vector2 currentVelocity;
+    public float agentSmoothTime = 0.5f;
+    public override Vector2 CalculateMove(Agent agent, List<Transform> context, Flock flock, Transform playerTransform)
+    {
+       // if (context.Count == 0)
+       //     return Vector2.zero;
+        Vector2 cohesionMove = Vector2.zero;
+     //   foreach (Transform item in context)
+     //   {
+     //       cohesionMove += (Vector2)item.position;
+     //   }
+     //
+     //   cohesionMove /= context.Count;
+
+        cohesionMove += (Vector2)playerTransform.position;
+
+       // cohesionMove /= 2;
+
+        cohesionMove -= (Vector2)agent.transform.position;
+
+        cohesionMove = Vector2.SmoothDamp(agent.transform.forward, cohesionMove, ref currentVelocity, agentSmoothTime/100f);
+
+        return cohesionMove;
+    }
+
+}

1533a16aa317d41d152dc83cbf971b723e442b44
MatisseWaghemans <55841549+MatisseWaghemans@users.noreply.github.com>
2020-04-11 11:45:19 +0200

    CHANGED MY SCENE
    GG

--- Lockdown_Gamejam_Project/Assets/Scripts/MainMenu/UIFunctions.cs
+++ Lockdown_Gamejam_Project/Assets/Scripts/MainMenu/UIFunctions.cs
@@ -2,10 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
 public class UIFunctions : MonoBehaviour
 {
     public Animator textAnimator;
+    public Animator ladaAnimator;
+    public Animator playerAnimator;
+    public Animator vignetteAnimator;
+
+
+    public GameObject UIfunctions;
+
+    private float timer;
+    private bool playClicked;
+
+    private void Update()
+    {
+        if (playClicked)
+        {
+            timer += Time.deltaTime;
+        }
+        if (timer >= 3)
+        {
+            StartGame();
+        }
+    }
     public void TriggerCameraShake()
     {
         textAnimator.SetTrigger("ScreenShake");
@@ -24,5 +44,13 @@ public class UIFunctions : MonoBehaviour
     {
         SceneManager.LoadScene("FirstLevel");
     }
+    public void PlayClicked()
+    {
+        playClicked = true;
+        ladaAnimator.SetTrigger("PlayPressed");
+        playerAnimator.SetTrigger("PlayPressed");
+        vignetteAnimator.SetTrigger("PlayPressed");
+    }
+
 
 }

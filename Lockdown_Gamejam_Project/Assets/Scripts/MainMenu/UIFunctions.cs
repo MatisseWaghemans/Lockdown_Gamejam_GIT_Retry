b26278080622bf0e479e4a1a917f02a1cde5d820
MatisseWaghemans <55841549+MatisseWaghemans@users.noreply.github.com>
2020-04-11 08:17:32 +0200

    Made startScreen
    ggg

---
+++ Lockdown_Gamejam_Project/Assets/Scripts/MainMenu/UIFunctions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIFunctions : MonoBehaviour
+{
+    public Animator textAnimator;
+    public void TriggerCameraShake()
+    {
+        textAnimator.SetTrigger("ScreenShake");
+    }
+
+    public void ResetTriggerCameraShake()
+    {
+        textAnimator.SetTrigger("StopScreenShake");
+
+    }
+    public void SetSnareTrigger()
+    {
+        textAnimator.SetTrigger("SnareTrigger");
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene("FirstLevel");
+    }
+
+}

c4ba1214cdb5a374aa222336c64f2499f2a25b8e
Maxim Eylenbosch <55977182+MaximEylenbosch@users.noreply.github.com>
2020-04-11 11:26:32 +0200

    ADDED: Vote booth mechanic

---
+++ Lockdown_Gamejam_Project/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    private int _score = 0;
+    public int Score { get => _score; set => _score = value; }
+
+    public void AddScore(int amount)
+    {
+        _score += amount;
+        Debug.Log(_score);
+    }
+}
